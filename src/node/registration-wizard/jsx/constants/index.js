bd55831b5f808bcf3a39229276863b4e8cc556cc
Ranjeet Kumar3 <ranjeetk3@chetu.com>
2017-02-20 23:17:18 +0530

    space issue resolved

--- src/node/registration-wizard/jsx/constants/index.js
+++ src/node/registration-wizard/jsx/constants/index.js
@@ -3,5 +3,5 @@ export { default as MONTHS } from './MONTHS';
 export { default as DAYS_OF_MONTH } from './DAYS_OF_MONTH';
 export { default as CREDIT_CARD_TYPES } from './CREDIT_CARD_TYPES';
 export { default as CARD_EXPIRY_MONTHS } from './CARD_EXPIRY_MONTHS';
-export {default as LANGUAGES } from './LANGUAGES';
-export {default as GENDERS } from './GENDERS'
+export { default as LANGUAGES } from './LANGUAGES';
+export { default as GENDERS } from './GENDERS'
