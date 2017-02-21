a9f2cdac3ef57706a258041ab967a5037144c00d
Roy Aslan <roy.aslan@kabbalah.com>
2017-02-21 14:53:06 -0500

    Removed env.js from gitignore. Env.js now detects if URL is local and determines API endpoint accordingly[D.

--- src/node/registration-wizard/jsx/env-example.js
+++
@@ -1,4 +0,0 @@
-export default {
-    POST_REGISTRATION_URL: 'https://idp.kabbalah.com/api-2017/users',
-		GET_TEACHERS_URL: 'https://idp.kabbalah.com/api-2017/get-teachers',
-}
---
+++ src/node/registration-wizard/jsx/env.js
@@ -0,0 +1,12 @@
+function getBaseURL() {
+    let hostname = window.location.hostname;
+    if (hostname.includes('local')) {
+        return 'http://localhost:3001';
+    }
+    return 'https://idp.kabbalah.com';
+}
+
+export default {
+    POST_REGISTRATION_URL:  getBaseURL() + '/api-2017/users',
+    GET_TEACHERS_URL: getBaseURL() + '/api-2017/get-teachers',
+}
